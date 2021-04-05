24f4b6c556c2ac5ef1a7c539caaa87716c04798a
Kyle Hale <khale@cs.iit.edu>
2021-04-05 21:48:58 +0000

    add first problem

--- Makefile
+++ Makefile
@@ -2,9 +2,13 @@ AS=lc3as
 ASM_SRC_FILES = $(shell find . -name "*.asm")
 ASM_OBJ_FILES:=$(notdir $(ASM_SRC_FILES:%.asm=%.obj))
 
+all: $(ASM_OBJ_FILES)
+
 $(ASM_OBJ_FILES): $(ASM_SRC_FILES)
 
 %.obj: %.asm
-	$(AS) $<
+	@echo "Assembling $<"
+	@$(AS) $<
+
 clean:
 	rm -f *.obj *.sym
--- problem1.asm
+++ problem1.asm
@@ -1,7 +1,41 @@
+;;
+;; Problem 1: Implement strcmp()
+;;   strcmp is a subroutine that compares the length of two strings.
+;;   The address of the first string is given in R0.
+;;   The address of the second string is given in R1.
+;;   If they are of equal length, R0 will hold 0 on return
+;;   If they are different length, R0 will hold 1 on return
+;;   You will fill in the strcmp() subroutine below. 
+;;
+
 .ORIG x3000
 
-    ADD R0, R0, #0
-;; TODO: fill me in 
+    LEA R0, STR1
+    LEA R1, STR1
+    JSR STRCMP
+    ADD R0, R0, #0      ; test return value
+    BRz EQUAL
+    LEA R0, DIFFSTR
+    PUTS
+    BRnzp DONE
+EQUAL
+    LEA R0, SAMESTR
+    PUTS
+DONE
     HALT
+
+STRCMP
+    ;;
+    ;; TODO: fill me in!
+    ;; 
+    RET
+
+
+;; Replace these with other strings to test your code
+STR1 .STRINGZ "placeholder1"
+STR2 .STRINGZ "placeholder1111"
+
+SAMESTR .STRINGZ "They are the same length"
+DIFFSTR .STRINGZ "They are different lengths"
 
 .END
