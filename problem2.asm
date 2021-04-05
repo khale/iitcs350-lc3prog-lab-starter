28aeeeb59739ac4c8a1e9c9ba1888624d00ba58e
Kyle Hale <khale@cs.iit.edu>
2021-04-05 22:04:42 +0000

    add problem 2

--- problem2.asm
+++ problem2.asm
@@ -1,7 +1,31 @@
+;;
+;; You will implement the daystring program below.
+;; You will first prompt the user to enter a number
+;; from 0 to 6, inclusive. Your program will output
+;; a string for the day of the week that the number corresponds to.
+;; The DAYSTRING subroutine accepts a number from 0 to 6 in R0
+;; and prints out the appropriate string. 
+;;
+;; Here is an example run;
+;;
+;; HINTS:
+;;  - understand how GETC works (and what data format is returned!)
+;;  - understand how to use strings
+
+
 .ORIG x3000
 
-    ADD R0, R0, #0
-;; TODO: fill me in 
+    LEA R0, PROMPT
+    PUTS
+    ;; TODO: fill me in!
     HALT
+
+    
+DAYSTRING
+    ;; TODO: fill me in!
+    RET
+
+PROMPT .STRINGZ "Input a number> "
+
 
 .END
